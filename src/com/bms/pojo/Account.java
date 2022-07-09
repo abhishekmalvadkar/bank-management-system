f5b3dfee6e7741f490d8166ce809f0e3664fe195
abhishek malvadkar ranoji <abhishek@dev.com>
2022-07-09 18:20:43 +0530

    Added Account Pojo class

---
+++ src/com/bms/pojo/Account.java
@@ -0,0 +1,49 @@
+package com.bms.pojo;
+
+public class Account {
+
+	private String accountNumber;
+	private Double balanace;
+	private String userName;
+	private Boolean isActive;
+
+	public Account(String accountNumber, Double balanace, String userName, Boolean isActive) {
+		this.accountNumber = accountNumber;
+		this.balanace = balanace;
+		this.userName = userName;
+		this.isActive = isActive;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Double getBalanace() {
+		return balanace;
+	}
+
+	public void setBalanace(Double balanace) {
+		this.balanace = balanace;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+}
